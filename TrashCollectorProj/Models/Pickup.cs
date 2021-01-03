c19e5c80d86f6e6bbf1db5499de1da5dcb625eb2
Zachary Inkster <z.j.inkster@gmail.com>
2021-01-02 18:23:28 -0600

    Created Customer Index page

--- TrashCollectorProj/Models/Pickup.cs
+++ TrashCollectorProj/Models/Pickup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace TrashCollectorProj.Models
 {
     public class Pickup
     {
+        [Key]
+        public int Id { get; set; }
+        public int Date { get; set; }
+        public int Time { get; set; }
+        [ForeignKey("IdentityUser")]
+        public int IdentityUser { get; set; }
     }
 }
